d5308e3acde3d383238d7ca26ff4b1de8832b28e
易墨 <507382865@qq.com>
2015-11-27 20:20:36 +0800

    创建第一个Demo:MVC但文件上传

---
+++ Example.FileUpload.MVC/Controllers/SingleUploadController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example.FileUpload.MVC.Controllers
+{
+    public class SingleUploadController : Controller
+    {
+        // GET: SingleUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                TempData["UploadMsg"] = "上传失败";
+            }
+            else
+            {
+                string ext = Path.GetExtension(file.FileName);
+                string fileName = Guid.NewGuid().ToString() + ext;
+                string savePath = Path.Combine(Common.WebSetting.DefaultUploadPath, fileName);
+                file.SaveAs(savePath);
+                TempData["UploadMsg"] = System.IO.File.Exists(savePath) ? "上传成功，保存在：" + savePath : "上传失败";
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Example.FileUpload.MVC/Models/FileModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.FileUpload.MVC.Models
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+    }
+}
