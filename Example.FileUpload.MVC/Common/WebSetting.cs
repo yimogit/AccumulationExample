04dad389ffee166481c02fcac6a6e3666b6cdd5f
易墨 <507382865@qq.com>
2015-11-27 20:41:57 +0800

    创建第一个Demo:MVC单文件上传

---
+++ Example.FileUpload.MVC/Common/WebSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Example.FileUpload.MVC.Common
+{
+    public static class WebSetting
+    {
+        public static string DefaultUploadPath
+        {
+            get {
+                if (!System.IO.Directory.Exists(HttpContext.Current.Request.MapPath("~/UploadFiles")))
+                    System.IO.Directory.CreateDirectory(HttpContext.Current.Request.MapPath("~/UploadFiles"));
+                return HttpContext.Current.Request.MapPath("~/UploadFile");            
+            }
+            set { DefaultUploadPath = value; }
+        }// = HttpContext.Current.Request.MapPath("~/UploadFile");
+    }
+}
